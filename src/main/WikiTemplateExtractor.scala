f6358f04fa725a37f9479ca3ea9bbef292398945
Permyak <nextzucker@gmail.com>
2016-03-10 00:19:18 +0500

    Add JSON parser.

--- src/main/WikiTemplateExtractor.scala
+++ src/main/WikiTemplateExtractor.scala
@@ -1,23 +1,30 @@
 package main
 
 import scala.io.{BufferedSource, Source}
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 object WikiTemplateExtractor {
-  def getDBpediaSparqlSelect(query:String):BufferedSource = {
+  def GetDBpediaSparqlSelect(query:String):BufferedSource = {
     val encodedQuery= java.net.URLEncoder.encode(query, "utf-8")
     return Source.fromURL(s"http://it.dbpedia.org/sparql?&query=$encodedQuery&format=json")
   }
 
-  def getPlayersCountForTemplate(templateURI:String):String = {
+  def GetPlayersCountFromJSON(JSON:String):Int = {
+    implicit val formats = DefaultFormats
+    (parse(JSON) \ "results" \ "bindings" \ "numero_calciatori" \ "value").extract[String].toInt
+  }
+
+  def GetPlayersCountForTemplate(templateURI:String):String = {
     val select ="select count(?s) as ?numero_calciatori " +
                 "where{" +
                 "?s a <http://dbpedia.org/ontology/SoccerPlayer>. " +
                 s"?s <http://it.dbpedia.org/property/wikiPageUsesTemplate> $templateURI}"
-    val html = getDBpediaSparqlSelect(select)
+    val html = GetDBpediaSparqlSelect(select)
     return html.mkString
   }
 
   def main(args: Array[String]): Unit = {
-    println(getPlayersCountForTemplate("<http://it.dbpedia.org/resource/Template:Sportivo>"))
+    println(GetPlayersCountFromJSON(GetPlayersCountForTemplate("<http://it.dbpedia.org/resource/Template:Sportivo>")))
   }
 }
